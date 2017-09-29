332f8e1c4e0619f30d18b524f8249572c98e88e0
piyawat <kct.thai@gmail.com>
2017-09-29 15:52:20 +0700

    Initial commit

---
+++ app/src/main/java/kct/piyawat/stockkct/fragment/MainFragment.java
@@ -0,0 +1,53 @@
+package kct.piyawat.stockkct.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import kct.piyawat.stockkct.R;
+
+/**
+ * Created by asus on 26/9/2560.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // NewRegister Controller
+        nerRegisterController();
+
+
+
+    }
+
+    private void nerRegisterController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);// Initial View
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Move to RegisterFragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentOfFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+    }
+} //Main Class
